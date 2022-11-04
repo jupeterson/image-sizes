f48c3e0a73282a002ab21b399a3a3c3155943057
Jonas Peterson <jonas.peterson@jellyhive.com>
2022-11-04 08:22:48 +0100

    calc number of files in images folder

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require('fs')
+const path = require('path')
+
+
 
 (
     async () => {
         try {
-            core.notice("Calling my action image-sizes")
+            const images = fs.readdir(
+                path.join(process.env.GITHUB_WORKSPACE, 'images')
+            )
+            core.notice("Calling my action image-sizes");
+            core.notice(`Number of images in repo: ${images.length}`)
         } catch (e) {
             core.setFailed(e.message)
         }
