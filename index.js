cb5c1b0558a90561a7526cc5c97ce5874fbf8c20
Jonas Peterson <jonas.peterson@jellyhive.com>
2022-11-04 11:57:44 +0100

    Better logging 3

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const sizeOf = require('image-size');
 (
     async () => {
         try {
-            const githubToken = core.getInput('repo_token');
-            const octokit = github.getOctokit(githubToken);
             core.notice("Executing my action image-sizes");
             console.log("All images in current directory and below" );
 
-            const tempImages = glob.sync( './**/*.jpg').map(fileName => {
+            const images = glob.sync( './**/*.jpg').map(fileName => {
                 const dimensions = sizeOf(fileName);
                 const fileInformation = {
                     imagePath: fileName,
@@ -23,23 +21,12 @@ const sizeOf = require('image-size');
                 console.log(fileInformation );
                 return fileInformation;
             });
-            console.log("All images in 'images' directory and below" );
-            const images = glob.sync( 'images/**/*.jpg').map(fileName => {
-                const dimensions = sizeOf(fileName);
-                const fileInformation = {
-                    imagePath: fileName,
-                    height: dimensions.height,
-                    width: dimensions.width
-                };
-                console.log(fileInformation );
-                return fileInformation;
-            });
-            core.notice(`Number of images in repo: ${images.length}`)
-            fs.writeFileSync('images/image-sizes.json', JSON.stringify(images));
+            core.notice(`Number of images in repo: ${images.length}`);
+            fs.writeFileSync('./public/images/image-sizes.json', JSON.stringify(images));
             
-            core.notice(`Written file 'image-sizes.json'`)
+            core.notice(`Written file 'image-sizes.json'`);
         } catch (e) {
-            core.setFailed(e.message)
+            core.setFailed(e.message);
         }
         
     }
