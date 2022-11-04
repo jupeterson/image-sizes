5753a75c9fc0343eb175d645ea5e43b79f34c851
Jonas Peterson <jonas.peterson@jellyhive.com>
2022-11-04 08:44:33 +0100

    fix 3

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const fs = require('fs');
 const path = require('path');
+const glob = require( 'glob' );
 
 (
     async () => {
         try {
-            const images = fs.readdir(
-                path.join(process.env.GITHUB_WORKSPACE, 'images')
-            );
+            // const images = await fs.readdir(
+            //     path.join(process.env.GITHUB_WORKSPACE, 'images')
+            // );
+            let images = [];
+            glob( 'images/**/*.jpg', function( err, files ) {
+                console.log( files );
+                images = [...images, ...files];
+            });
             core.notice("Calling my action image-sizes");
             core.notice(`Number of images in repo: ${images.length}`)
         } catch (e) {
